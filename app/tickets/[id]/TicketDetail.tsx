a6b0864134297562ac14a2100f90a4d647eb4447
Explorarthecreator <fredadejiemma99@gmail.com>
2024-09-27 14:42:02 +0100

    Installed react-markdown to enable show the ticket deescription properly

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,14 +1,75 @@
 import { Ticket } from "@prisma/client";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TicketsStatusBadge from "@/components/TicketsStatusBadge";
+import TicketsPriority from "@/components/TicketsPriority";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import ReactMarkDown from "react-markdown";
 
 interface Props {
   ticket: Ticket;
 }
 const TicketDetail = ({ ticket }: Props) => {
   return (
-    <div>
-      <p>{ticket.title}</p>
-      <p>{ticket.description}</p>
+    <div className="lg:grid lg:grid-cols-4">
+      <Card className="mx-4 mb-4  lg:col-span-3 lg:mr-4">
+        <CardHeader>
+          <div className="flex justify-between mb-3">
+            <TicketsStatusBadge status={ticket.status} />
+            <TicketsPriority priority={ticket.priority} />
+          </div>
+          <CardTitle>{ticket.title}</CardTitle>
+          <CardDescription>
+            Created:{" "}
+            {ticket.createdAt.toLocaleDateString("en-Us", {
+              year: "2-digit",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "numeric",
+              minute: "2-digit",
+              hour12: true,
+            })}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="prose dark:prose-invert">
+          <ReactMarkDown>{ticket.description}</ReactMarkDown>
+        </CardContent>
+        <CardFooter>
+          Updated:{" "}
+          {ticket.updatedAt.toLocaleDateString("en-Us", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+          })}
+        </CardFooter>
+      </Card>
+
+      <div className="mx-4 flex lg:flex-col lg:mx-0 gap-2">
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({ variant: "default" })}`}
+        >
+          Edit Ticket
+        </Link>
+
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({ variant: "default" })}`}
+        >
+          Delete Ticket
+        </Link>
+      </div>
     </div>
   );
 };
